b9bbc3b97f727017263278f422020104a9b15084
totzYuta <zfbfmtmxaz@gmail.com>
2014-09-27 10:42:58 +0900

    Fetch user's data as json file. refs #3

--- setting_twit.rb
+++ setting_twit.rb
@@ -2,6 +2,7 @@ require 'oauth'
 require 'oauth/consumer'
 require 'twitter'
 require 'pp'
+require 'json'
 
 CONSUMER_KEY = "TvEDulP9wjxcAkpPS2r1ux4Fr"
 CONSUMER_SECRET = "t3AO41ngBpr4I30I0bkYNFXXlQtjCwahAgNXoiEtYHkaWxtIcu"
@@ -24,9 +25,16 @@ pin = (gets.chomp).to_i
 
 @access_token = @request_token.get_access_token(:oauth_verifier => pin)
 
-io = File.open("setting.rb", "w")
-io.puts "ACCESS_TOKEN = \"#{@access_token.token}\""
-io.puts "ACCESS_TOKEN_SECRET = \"#{@access_token.secret}\""
+endpoint = OAuth::AccessToken.new(@consumer, @access_token.token, @access_token.secret)
+
+# GET user account information as json file
+response = endpoint.request(:get, 'https://api.twitter.com/1.1/account/settings.json')
+result = JSON.parse(response.body)
+p result
+
+io = File.open("setting_#{@access_token.token}.rb", "w")
+io.puts "ACCESS_TOKEN_#{@access_token.token} = \"#{@access_token.token}\""
+io.puts "ACCESS_TOKEN_SECRET_#{@access_token.token} = \"#{@access_token.secret}\""
 io.close
 
 puts "================="
